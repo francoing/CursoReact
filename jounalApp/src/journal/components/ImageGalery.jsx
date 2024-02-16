8e2a6d0319cf701be464afdce7916246eb3e7b12
Franco.Montti <franco.montti@suizoargentina.com>
2024-02-16 11:27:48 -0300

    cambios para hacer deploy

--- jounalApp/src/journal/components/ImageGalery.jsx
+++ jounalApp/src/journal/components/ImageGalery.jsx
@@ -3,19 +3,21 @@ import { ImageList, ImageListItem } from "@mui/material";
 
 export const ImageGalery = ({images})=> {
   return (
-    <ImageList sx={{ width: '100%', height: 500 }} cols={4} rowHeight={200}>
-      {images.map((image) => (
-        <ImageListItem key={image}>
-          <img
-            srcSet={`${image}?w=164&h=164&fit=crop&auto=format&dpr=2 2x`}
-            src={`${image}?w=164&h=164&fit=crop&auto=format`}
-            alt='imagen de prueba'
-            loading="lazy"
-          />
-        </ImageListItem>
-      ))}
-    </ImageList>
-  );
+      <ImageList sx={{ width: '100%', height: 500 }} cols={4} rowHeight={200}>
+        {images && images.map((image) => (
+          <ImageListItem key={image}>
+            <img
+              srcSet={`${image}?w=164&h=164&fit=crop&auto=format&dpr=2 2x`}
+              src={`${image}?w=164&h=164&fit=crop&auto=format`}
+              alt='imagen de prueba'
+              loading="lazy"
+            />
+          </ImageListItem>
+        ))}
+      </ImageList>
+      );
+   
+    
 }
 
    
